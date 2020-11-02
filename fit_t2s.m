0f02b8346dff1de9231f4c39eea6deb1518be2ac
cjl2007 <46632198+cjl2007@users.noreply.github.com>
2020-11-02 07:35:47 -0500

    Add files via upload

---
+++ fit_t2s.m
@@ -0,0 +1,169 @@
+
+% add resources;
+addpath(genpath(resources));
+
+% clean dir.
+system(['rm -rf ' Subdir '/func/t2star/']);
+system(['mkdir ' Subdir '/func/t2star/']);
+
+% define the number of sessions;
+sessions = dir([Subdir '/func/rest/session_*']);
+
+% load echo times;
+te = load([Subdir '/func/rest/session_1/run_1/te.txt']); % lets assume that all scans have the same te; this may not be true in practice; user beware.
+te = double(te); % convert to double
+
+% create and load a brain mask;
+mask = niftiread([Subdir '//func/xfms/rest/T1w_nonlin_brain_2mm_mask.nii.gz']);
+dims = size(mask); % data dims;
+mask = reshape(mask,[dims(1)*dims(2)*dims(3),1]);
+brain_voxels = find(mask==1); % define all in-brain voxels;
+
+% read in example header information;
+info = niftiinfo([Subdir '/func/xfms/rest/T1w_nonlin_brain_2mm_mask.nii.gz']);
+
+% start parpool;
+pool = parpool('local',ncores);
+
+% sweep the scans;
+for s = 1:length(sessions)
+    
+    % this is the number of runs for this session;
+    runs = dir([Subdir '/func/rest/session_' num2str(s) '/run_*']);
+    
+    % sweep the runs;
+    for r = 1:length(runs)
+
+        % sweep the TE
+        parfor i = 1:length(te)
+           
+            % calculate a temporal average;
+            system(['fslmaths ' Subdir '/func/rest/session_' num2str(s) '/run_' num2str(r) '/Rest_E' num2str(i) '_nonlin.nii.gz -Tmean ' Subdir '/workspace/temp_e' num2str(i) '_s' num2str(s) '_r' num2str(r) '.nii.gz']); 
+
+        end
+
+    end
+
+end
+
+% sweep the TE
+parfor i = 1:length(te)
+    
+    % merge all of the individual echoes;
+    system(['fslmerge -t ' Subdir '/workspace/temp_avg_e' num2str(i) '.nii.gz ' Subdir '/workspace/temp_e' num2str(i) '*.nii.gz']);
+    
+    % average across all echoes;
+    system(['fslmaths ' Subdir '/workspace/temp_avg_e' num2str(i) '.nii.gz -Tmean ' Subdir '/workspace/temp_avg_e' num2str(i) '.nii.gz']);
+    
+    % read in the .nii
+    nii = niftiread([Subdir '/workspace/temp_avg_e' num2str(i) '.nii.gz']); 
+    
+    % data dims;
+    dims = size(nii); 
+    
+    % log this average echo;
+    data(:,i) = reshape(double(nii),[dims(1)*dims(2)*dims(3),1]);
+    
+end
+
+% move "average TEs" image to t2s dir.;
+system(['fslmerge -t ' Subdir '/func/t2star/Avg_TEs.nii.gz ' Subdir '/workspace/temp_avg_e*.nii.gz']);
+    
+% turn off
+% annoying warnings
+warning('off','all');
+
+% S = S0 * exp(-t * R2*)
+mefun = @(b,x)(b(1)*exp(-b(2)*x));
+
+% sweep all in-brain voxels;
+parfor i = 1:length(brain_voxels)
+    
+    % dont trust this voxel if signal
+    % is not decreasing across echoes;
+    if issorted(data(brain_voxels(i),:),'descend')
+        
+        % fit a monoexponential decay model (slower but more accurate);
+        [b_init] = regress(log(abs(data(brain_voxels(i),:))+1)',[ones(length(te),1) (te*-1)']); % use log-lin fit as initial guess;
+        mdl = fitnlm(te,data(brain_voxels(i),:),mefun,[data(brain_voxels(i),1) b_init(2)]);
+        
+        % log values;
+        a(i) = 1 / mdl.Coefficients.Estimate(2); % note: T2* = 1 / R2*
+        b(i) = mdl.Rsquared.Adjusted; % R2
+        
+    else
+        
+        % log "place-
+        % holder" values;
+        a(i) = 0; % T2*
+        b(i) = 0; % R2
+        
+    end
+    
+end
+
+% preallocate
+% output variables;
+t2s = zeros(size(mask));
+gof = zeros(size(mask)); 
+
+% log values from parfor loop;
+t2s(brain_voxels) = a; % T2*
+gof(brain_voxels) = b; % R2
+
+% adjust header info;
+info.Datatype='double';
+info.BitsPerPixel = 64;
+
+% cap "extreme" values
+t2s(t2s > 500) = 0; % this cutoff is used by tedana.
+t2s(t2s < 0) = 0; % remove negative values 
+t2s(gof < 0.8) = 0; % remove "bad" fits;
+
+% reshape, write, and compress t2s map;
+t2s = reshape(t2s,[dims(1),dims(2),dims(3)]);
+niftiwrite(t2s,[Subdir '/func/t2star/T2star_nonlin'],info);
+system(['gzip -f ' Subdir '/func/t2star/T2star_nonlin.nii']);
+
+% reshape, write, and compress t2s map;
+gof = reshape(gof,[dims(1),dims(2),dims(3)]);
+niftiwrite(gof,[Subdir '/func/t2star/R2_nonlin'],info);
+system(['gzip -f ' Subdir '/func/t2star/R2_nonlin.nii']);
+
+% fill in bad values;
+system(['wb_command -volume-dilate ' Subdir '/func/t2star/T2star_nonlin.nii.gz 10 NEAREST  ' Subdir '/func/t2star/T2star_nonlin.nii.gz -data-roi ' Subdir '/func/xfms/rest/T1w_nonlin_brain_2mm.nii.gz']);
+
+% now, oc-me
+
+% create and load a brain mask;
+t2s = niftiread([Subdir '/func/t2star/T2star_nonlin.nii.gz']);
+dims = size(t2s); % data dims;
+t2s = reshape(t2s,[dims(1)*dims(2)*dims(3),1]);
+
+% load all the average echoes;
+data = niftiread([Subdir '/func/t2star/Avg_TEs.nii.gz']);
+dims = size(data); % data dims;
+data = reshape(data,[dims(1)*dims(2)*dims(3),dims(4)]);
+
+% preallocate weights for te;
+wte = zeros(size(data,1),length(te));
+
+% sweep all points in the brain;
+for i = 1:length(brain_voxels)
+    
+    % sweep through te
+    for ii = 1:length(te)
+        wte(brain_voxels(i),ii) = te(ii) * exp( ( te(ii) * -1 ) / t2s(brain_voxels(i)) );
+    end
+    
+    % normalize weights accross te;
+    wte(brain_voxels(i),:) = wte(brain_voxels(i),:) / norm(wte(brain_voxels(i),:),1);
+     
+end
+
+% combine the images using weights;
+oc_me = reshape(sum( double(wte) .* double(data) , 2),[dims(1),dims(2),dims(3),1]);
+
+% write out non-optimally combined image and weights; 
+niftiwrite(oc_me,[Subdir '/func/t2star/OCME'],info);
+system(['gzip -f ' Subdir '/func/t2star/OCME.nii']); % zip oc-me file; 
---
+++ func_denoise_t2s.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# CJL; (cjl2007@med.cornell.edu)
+
+Subject=$1
+StudyFolder=$2
+Subdir="$StudyFolder"/"$Subject"
+RESOURCES=$3
+NTHREADS=$4
+FS="$RESOURCES/FS" # dir. with FreeSurfer (FS) atlases 
+FSL="$RESOURCES/FSL" # dir. with FSL (FSL) atlases 
+Sigma=`echo "5 / ( 2 * ( sqrt ( 2 * l ( 2 ) ) ) )" | bc -l` # define sigma
+
+# fresh workspace dir.
+rm -rf "$Subdir"/workspace/ > /dev/null 2>&1 
+mkdir "$Subdir"/workspace/ > /dev/null 2>&1 
+
+# calculate t2* map ;
+cp -rf "$RESOURCES"/fit_t2s.m \
+"$Subdir"/workspace/temp.m
+
+# define some Matlab variables
+echo Subdir=["'$Subdir'"]  | cat - "$Subdir"/workspace/temp.m > temp && mv temp "$Subdir"/workspace/temp.m
+echo resources=["'$RESOURCES'"] | cat - "$Subdir"/workspace/temp.m >> temp && mv temp "$Subdir"/workspace/temp.m
+echo ncores="$NTHREADS" | cat - "$Subdir"/workspace/temp.m >> temp && mv temp "$Subdir"/workspace/temp.m 
+
+cd "$Subdir"/workspace/ 
+matlab -noFigureWindows -nodesktop -nosplash -r "temp; exit" > /dev/null 2>&1  
+rm "$Subdir"/workspace/temp.m > /dev/null 2>&1  # remove the temp. script
+rm "$Subdir"/workspace/* > /dev/null 2>&1  # clean slate
+
+# sweep through hemispheres;
+for hemisphere in lh rh ; do
+
+	# define two different 
+	# ways of saying left and right;
+	if [ $hemisphere = "lh" ] ; then
+		Hemisphere="L"
+	elif [ $hemisphere = "rh" ] ; then
+		Hemisphere="R"
+	fi
+
+	# define all of the the relevant surfaces;
+	MIDTHICK="$Subdir"/anat/MNINonLinear/Native/$Subject.$Hemisphere.midthickness.native.surf.gii
+	MIDTHICK_FSLR32k="$Subdir"/anat/MNINonLinear/fsaverage_LR32k/$Subject.$Hemisphere.midthickness.32k_fs_LR.surf.gii
+	ROI="$Subdir"/anat/MNINonLinear/Native/$Subject.$Hemisphere.roi.native.shape.gii
+	ROI_FSLR32k="$Subdir"/anat/MNINonLinear/fsaverage_LR32k/$Subject.$Hemisphere.atlasroi.32k_fs_LR.shape.gii
+	REG_MSMSulc="$Subdir"/anat/MNINonLinear/Native/$Subject.$Hemisphere.sphere.MSMSulc.native.surf.gii
+	REG_MSMSulc_FSLR32k="$Subdir"/anat/MNINonLinear/fsaverage_LR32k/$Subject.$Hemisphere.sphere.32k_fs_LR.surf.gii
+
+	# create temporary cortical ribbon image;
+    wb_command -volume-math "(ribbon > ($ribbon - 0.01)) * (ribbon < ($ribbon + 0.01))" \
+    "$Subdir"/anat/MNINonLinear/temp_ribbon.nii.gz -var ribbon "$Subdir"/anat/MNINonLinear/ribbon.nii.gz
+
+	# map t2s map from volume to surface using "myelin" method;
+	wb_command -volume-to-surface-mapping "$Subdir"/func/t2star/T2star_nonlin.nii.gz "$MIDTHICK" \
+	"$Subdir"/func/t2s/"$hemisphere".native.shape.gii -myelin-style "$Subdir"/anat/MNINonLinear/temp_ribbon.nii.gz \
+	"$Subdir"/anat/MNINonLinear/Native/"$Subject"."$Hemisphere".thickness.native.shape.gii
+	rm "$Subdir"/anat/MNINonLinear/temp_ribbon.nii.gz "$Sigma"
+
+	# dilate metric file 10mm in geodesic space;
+	wb_command -metric-dilate "$Subdir"/func/t2s/"$hemisphere".native.shape.gii \
+	"$MIDTHICK" 10 "$Subdir"/func/t2s/"$hemisphere".native.shape.gii -nearest
+
+	# mask out medial wall in native mesh;  
+	wb_command -metric-mask "$Subdir"/func/t2s/"$hemisphere".native.shape.gii \
+	"$ROI" "$Subdir"/func/t2s/"$hemisphere".native.shape.gii 
+
+	# resample metric data from native mesh to fs_LR_32k mesh;
+	wb_command -metric-resample "$Subdir"/func/t2s/"$hemisphere".native.shape.gii "$REG_MSMSulc" \
+	"$REG_MSMSulc_FSLR32k" ADAP_BARY_AREA "$Subdir"/func/t2s/"$hemisphere".32k_fs_LR.shape.gii \
+	-area-surfs "$MIDTHICK" "$MIDTHICK_FSLR32k" -current-roi "$ROI"
+
+	# mask out medial wall on fs_LR_32k mesh;
+	wb_command -metric-mask "$Subdir"/func/t2s/"$hemisphere".32k_fs_LR.shape.gii \
+	"$ROI_FSLR32k" "$Subdir"/func/t2s/"$hemisphere".32k_fs_LR.shape.gii
+
+done 
+
+# combine hemispheres and subcortical structures into a single CIFTI files; 
+wb_command -cifti-create-dense-timeseries "$Subdir"/func/t2star/T2star.dtseries.nii -volume "$Subdir"/func/t2star/T2star_nonlin.nii.gz "$Subdir"/anat/MNINonLinear/ROIs/Atlas_ROIs.2.nii.gz \
+-left-metric "$Subdir"/func/t2star/lh.32k_fs_LR.shape.gii -roi-left "$Subdir"/anat/MNINonLinear/fsaverage_LR32k/"$Subject".L.atlasroi.32k_fs_LR.shape.gii \
+-right-metric "$Subdir"/func/t2star/rh.32k_fs_LR.shape.gii -roi-right "$Subdir"/anat/MNINonLinear/fsaverage_LR32k/"$Subject".R.atlasroi.32k_fs_LR.shape.gii
+rm "$Subdir"/func/t2s/*shape* # remove left over files 
+
+# smooth with geodesic (for surface data) and Euclidean (for volumetric data) Gaussian kernels; sigma = 2.55
+wb_command -cifti-smoothing "$Subdir"/func/t2star/T2star.dtseries.nii 2.55 2.55 COLUMN "$Subdir"/func/t2star/T2star_s2.55.dtseries.nii \
+-left-surface "$Subdir"/anat/MNINonLinear/fsaverage_LR32k/"$Subject".L.midthickness.32k_fs_LR.surf.gii -right-surface "$Subdir"/anat/MNINonLinear/fsaverage_LR32k/"$Subject".R.midthickness.32k_fs_LR.surf.gii 
+ 
+
